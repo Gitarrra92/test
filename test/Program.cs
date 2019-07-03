b08fbf7157adff4de85bb8d60dadef05100deb1d
Natalia <pilewska.natalia@gmail.com>
2019-07-03 18:41:29 +0200

    Początkowe zaewidencjonowanie modułu test

---
+++ test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoje imie ");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello, {0} lets play a game", name );
+
+            int correctNumber = 7;
+            int guess = 0;
+            Console.WriteLine("Guess a number between 0-10");
+
+            while (guess != correctNumber) {
+                string inputUser = Console.ReadLine();
+                guess = Int32.Parse(inputUser);
+
+                if (guess != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number");
+                    Console.ResetColor();
+                } else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
